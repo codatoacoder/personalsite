8f631362d881edcca4ac17680f99371c389a85c2
TOx <tox@local>
2021-10-25 22:34:40 -0700

    Next Phase - Static Generation

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -14,7 +15,12 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
-      </section>
+      </section> 
+      <nav>
+        <Link href='/posts/first-post'>
+          <a>Posts</a>
+        </Link>
+      </nav>
     </Layout>
   )
 }
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Alert from '../../components/alert'
 import Layout from '../../components/layout'
 
 export default function FirstPost() {
@@ -10,11 +11,12 @@ export default function FirstPost() {
                     <title>First Post</title>
                 </Head>
                 <h1>First Post</h1>
-                <h2>
-                    <Link href="/">
-                        <a>Back to home</a>
-                    </Link>
-                </h2>
+                <h2>Sub title</h2>
+                <p>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                </p>
+                <Alert success>Vouz!</Alert>
+                <Alert error>Alez!</Alert>
             </Layout>
         </>
     )
