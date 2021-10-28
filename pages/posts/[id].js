f88b15cf1010446ca347caf933804c5a2572b9dd
TOx <tox@local>
2021-10-28 00:10:52 -0700

    Got ahead!

---
+++ pages/posts/[id].js
@@ -0,0 +1,20 @@
+import Layout from '../../components/layout'
+import { getAllPostIds } from '../../lib/posts'
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export default function Post() {
+  return <Layout>...</Layout>
+}
+
+export async function getStaticProps({ params }) {
+    // Fetch necessary data for the blog post using params.id
+
+}
+
