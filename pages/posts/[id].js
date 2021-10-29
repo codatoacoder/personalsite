ac0354f0d5f4aa2f0d47d11944a110459589563c
TOx <tox@local>
2021-10-28 23:06:32 -0700

    After my version-broke site

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,25 @@
 import Layout from '../../components/layout'
-import { getAllPostIds } from '../../lib/posts'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
+
+export default function Post() {
+  return 
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+}
 
 export async function getStaticPaths() {
   const paths = getAllPostIds()
@@ -8,13 +28,3 @@ export async function getStaticPaths() {
     fallback: false
   }
 }
-
-export default function Post() {
-  return <Layout>...</Layout>
-}
-
-export async function getStaticProps({ params }) {
-    // Fetch necessary data for the blog post using params.id
-
-}
-
